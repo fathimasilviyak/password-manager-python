fbb0eaa14547ded8acb6450434bbdb8b88468f10
fathimasilviyak <silviyakvm@gmail.com>
2023-04-08 00:16:00 +0530

    add labels, inputs and buttons

--- main.py
+++ main.py
@@ -3,11 +3,38 @@ from tkinter import *
 # UI setup
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
-canvas = Canvas(width=200, height=200)
+window.config(padx=50, pady=50)
+canvas = Canvas(height=200, width=200)
 logo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo)
-canvas.pack()
+canvas.grid(row=0, column=1)
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+# Entries
+website_input = Entry(width=51)
+website_input.grid(row=1, column=1, columnspan=2)
+website_input.focus()
+email_input = Entry(width=51)
+email_input.grid(row=2, column=1, columnspan=2)
+email_input.insert(0, "anu@gmail.com")
+password_input = Entry(width=32)
+password_input.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=44)
+add_button.grid(row=4, column=1, columnspan=2)
+
+
+
+
 
 
 window.mainloop()
