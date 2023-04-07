98d67b5c832bc4e5d7fab7084661b15b0af90a0c
fathimasilviyak <silviyakvm@gmail.com>
2023-04-07 20:31:22 +0530

    UI setup

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# UI setup
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+canvas = Canvas(width=200, height=200)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.pack()
+
+
+window.mainloop()
