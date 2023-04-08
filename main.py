693b81912f363f08e4c97db6eebe64dc03194dbf
fathimasilviyak <silviyakvm@gmail.com>
2023-04-08 13:42:08 +0530

    generate password

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+
+# Password Generator
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def generate_password():
+    # password_list = []
+
+    password_letter = [choice(letters) for i in range(randint(8, 10))]
+    password_symbol = [choice(symbols) for i in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for i in range(randint(2, 4))]
+
+    password_list =  password_letter + password_symbol + password_numbers
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_input.insert(0, password)
+
 
 # Save password
 def save_password():
     website = website_input.get()
     email = email_input.get()
-    password = password_input.get()
-    if len(website) == 0 or len(password) == 0:
+    password_data = password_input.get()
+    if len(website) == 0 or len(password_data) == 0:
         messagebox.showerror(title="Oops", message="Please make sure you haven't left any fields empty!")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}\n"
-                                                  f"Password: {password}\n Is it ok to save?")
+                                                              f"Password: {password_data}\n Is it ok to save?")
         if is_ok:
             with open("data.txt", mode="a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_input.delete(0,END)
-                password_input.delete(0,END)
+                data_file.write(f"{website} | {email} | {password_data}\n")
+                website_input.delete(0, END)
+                password_input.delete(0, END)
                 website_input.focus()
-
-
 
 
 # UI setup
@@ -48,7 +71,7 @@ password_input = Entry(width=32)
 password_input.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=44, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2)
